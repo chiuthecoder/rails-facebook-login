25f75d84f554bca017ebe330bedb733ed9dbf398
Chiuhua Chen <chiuhua.chen@gmail.com>
2015-06-22 12:43:35 -0700

    first commit

---
+++ config/initializers/constants.rb
@@ -0,0 +1,5 @@
+#config/initializers/constants.rb
+#indicate your app id and secret key provided by Facebook
+APP_ID= '127216317609721'
+APP_SECRET= '6eb7f5d1e0268a3823894b0ab8cf0a21'
+SITE_URL = 'http://localhost:3000' 
